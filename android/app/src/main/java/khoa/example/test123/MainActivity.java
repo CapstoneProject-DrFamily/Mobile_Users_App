5da03f834e02249c55b73e1c3cd725e15cab41b9
KhoaDTAK07 <53048122+KhoaDTAK07@users.noreply.github.com>
2020-12-08 15:00:51 +0700

    firt commit
    firt commit

---
+++ android/app/src/main/java/khoa/example/test123/MainActivity.java
@@ -0,0 +1,6 @@
+package khoa.example.test123;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/App.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  //render
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
