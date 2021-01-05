511b1b354eb3b32e29b4c940132212a0f4043baf
BuiThongHoangDuc <cubiheo5@gmail.com>
2021-01-05 14:28:44 +0700

    fix base view

--- lib/screens/base_view.dart
+++ lib/screens/base_view.dart
@@ -4,7 +4,7 @@ import 'package:scoped_model/scoped_model.dart';
 class BaseView<T extends Model> extends StatelessWidget {
   final ScopedModelDescendantBuilder<T> _builder;
 
-  BaseView({ScopedModelDescendantBuilder builder}) : _builder = builder;
+  BaseView({ScopedModelDescendantBuilder<T> builder}) : _builder = builder;
   @override
   Widget build(BuildContext context) {
     return ScopedModel<T>(
