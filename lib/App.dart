d7cb748b419764d4792a5ff453c278f88399e720
BuiThongHoangDuc <cubiheo5@gmail.com>
2021-01-29 12:41:49 +0700

    commit merge conflicts

--- lib/App.dart
+++ lib/App.dart
@@ -1,9 +1,4 @@
-import 'package:drFamily_app/screens/health_record_screen.dart';
-import 'package:drFamily_app/screens/home/find_doctor/list_doctor_page.dart';
-import 'package:drFamily_app/screens/home/find_doctor/map_page.dart';
-import 'package:drFamily_app/screens/landing_page/lading_page.dart';
 import 'package:drFamily_app/screens/login/login_page.dart';
-import 'package:drFamily_app/screens/prescription_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
