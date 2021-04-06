fb966067434cf14ba92824010665bcf564236d17
BuiThongHoangDuc <cubiheo5@gmail.com>
2021-04-06 16:12:58 +0700

    merge master

--- lib/view_model/setting_vm/profile_screen_view_model.dart
+++ lib/view_model/setting_vm/profile_screen_view_model.dart
@@ -211,9 +211,9 @@ class ProfileScreenViewModel extends BaseModel {
     _bloodTpeController.text = _additionInfoModel.bloodType;
 
     int convertHeight = _additionInfoModel.height.toInt();
-    _heightController.text = convertHeight.toString() + "cm";
+    _heightController.text = convertHeight.toString();
 
-    _weightController.text = _additionInfoModel.weight.toString() + "kg";
+    _weightController.text = _additionInfoModel.weight.toString();
 
     this._isLoading = false;
     notifyListeners();
@@ -371,19 +371,14 @@ class ProfileScreenViewModel extends BaseModel {
       print("height: " + height);
       print("weight: " + weight);
 
-      double newWeight;
-      if (_weightController.text.length <= 5) {
-        newWeight = double.parse(_weightController.text.substring(0, 3));
-      } else {
-        newWeight = double.parse(_weightController.text.substring(0, 4));
-      }
-
-      print("newWeight: " + newWeight.toString());
-
       _additionInfoModel = new AdditionInfoModel(
         patientId: patientID,
-        height: double.parse(_heightController.text.substring(0, 3)),
-        weight: newWeight,
+        height: (_heightController.text == "")
+            ? 0
+            : double.parse(_heightController.text),
+        weight: (_weightController.text == "")
+            ? 0
+            : double.parse(_weightController.text),
         bloodType: bloodTpeController.text,
         relationship: relationship,
       );
