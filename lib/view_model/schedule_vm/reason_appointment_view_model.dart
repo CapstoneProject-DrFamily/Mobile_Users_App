50c223f776d42c5ac1ba69177bb3f3397dd3e83f
BuiThongHoangDuc <cubiheo5@gmail.com>
2021-05-02 20:04:13 +0700

    udpate schedule

--- lib/view_model/schedule_vm/reason_appointment_view_model.dart
+++ lib/view_model/schedule_vm/reason_appointment_view_model.dart
@@ -87,6 +87,7 @@ class ReasonAppointmentViewModel extends BaseModel {
     print("Appointment time :" + appointmentTime);
     String formattedDate = DateFormat('dd-MM-yyyy - HH:mm')
         .format(DateTime.parse(appointmentTime));
+    print("appoitnment Time format $formattedDate");
 
     ScheduleModel schedule = ScheduleModel(
         appointmentTime: appointmentTime,
