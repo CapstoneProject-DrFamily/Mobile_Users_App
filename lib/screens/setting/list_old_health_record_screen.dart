59a1990d81337f795bf2b46aaa45935345fc0468
KhoaDTAK07 <53048122+KhoaDTAK07@users.noreply.github.com>
2021-05-18 08:51:33 +0700

    fix UI list old PHR

--- lib/screens/setting/list_old_health_record_screen.dart
+++ lib/screens/setting/list_old_health_record_screen.dart
@@ -1,6 +1,7 @@
 import 'package:drFamily_app/screens/setting/old_health_record_screen.dart';
 import 'package:drFamily_app/screens/share/base_view.dart';
 import 'package:drFamily_app/view_model/setting_vm/old_health_record_view_model.dart';
+import 'package:drFamily_app/widgets/common/app_image.dart';
 import 'package:drFamily_app/widgets/common/fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -41,42 +42,69 @@ class ListOldHealthRecordScreen extends StatelessWidget {
                         backgroundColor: Colors.white,
                       ),
                     )
-                  : Container(
-                      child: Column(
-                        children: <Widget>[
-                          Flexible(
-                            child: ListView.builder(
-                                itemCount: model.listOldHealthRecord.length,
-                                itemBuilder: (BuildContext context, int index) {
-                                  return ListTile(
-                                    title: Text("Date: " +
-                                        DateFormat('dd-MM-yyyy')
-                                            .format(DateTime.parse(model
-                                                .listOldHealthRecord[index]
-                                                .insDatetime))
-                                            .toString()),
-                                    subtitle: Text("Created by: " +
-                                        model.listOldHealthRecord[index].insBy),
-                                    trailing: IconButton(
-                                      icon: Icon(Icons.chevron_right),
-                                      onPressed: () => Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) =>
-                                              OldHealthRecordScreen(
-                                            healthRecordID: model
-                                                .listOldHealthRecord[index]
-                                                .healthRecordID,
+                  : (model.listOldHealthRecord.length != 0)
+                      ? Container(
+                          child: Column(
+                            children: <Widget>[
+                              Flexible(
+                                child: ListView.builder(
+                                    itemCount: model.listOldHealthRecord.length,
+                                    itemBuilder:
+                                        (BuildContext context, int index) {
+                                      return ListTile(
+                                        title: Text("Date: " +
+                                            DateFormat('dd-MM-yyyy')
+                                                .format(DateTime.parse(model
+                                                    .listOldHealthRecord[index]
+                                                    .insDatetime))
+                                                .toString()),
+                                        subtitle: Text("Created by: " +
+                                            model.listOldHealthRecord[index]
+                                                .insBy),
+                                        trailing: IconButton(
+                                          icon: Icon(Icons.chevron_right),
+                                          onPressed: () => Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  OldHealthRecordScreen(
+                                                healthRecordID: model
+                                                    .listOldHealthRecord[index]
+                                                    .healthRecordID,
+                                              ),
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                    ),
-                                  );
-                                }),
+                                      );
+                                    }),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                    );
+                        )
+                      : Container(
+                          child: Column(
+                            children: <Widget>[
+                              Center(
+                                child: Container(
+                                  height: 100,
+                                  width: 100,
+                                  margin: EdgeInsets.only(
+                                    top: MediaQuery.of(context).size.height /
+                                        3.5,
+                                    bottom: 20.0,
+                                  ),
+                                  child: Image.asset(
+                                    PHR_ICON,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                "You don't have any old Personal Health Record",
+                              ),
+                            ],
+                          ),
+                        );
             },
           );
         },
